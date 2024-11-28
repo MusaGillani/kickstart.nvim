e64fe9c917c52f70b943638ee198a4a5060de4f6
Musa Gillani <musagillaniwork@gmail.com>
2024-11-28 06:31:22 +0500

    add new color scheme and fix color scheme init
    move init colorscheme to last to load it properly

--- lua/custom/plugins/moonfly-theme.lua
+++ lua/custom/plugins/moonfly-theme.lua
@@ -3,13 +3,4 @@ return {
   name = 'moonfly',
   lazy = false,
   priority = 1000,
-  init = function()
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'moonfly'
-
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
-  end,
 }
